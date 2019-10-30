48d8f5a55eb52ff2960d158816b3d0eb1ee88fc2
Mani Kumar K V <mkanamarlapudi@nasinsurance.com>
2019-10-30 18:32:17 +0530

    get All code

--- functions/pujas/getAll.ts
+++ functions/pujas/getAll.ts
@@ -1,11 +1,11 @@
 import { APIGatewayEvent, APIGatewayEventRequestContext } from 'aws-lambda';
 import 'source-map-support/register';
+const dbModels = require('../../models/index')
 
 export const getAll: any = async (event: APIGatewayEvent, _context: APIGatewayEventRequestContext) => {
+   const x =  await dbModels.Puja.findAll();
+   console.log(event.body)
   return {
-    body: JSON.stringify({
-      message: 'get All',
-      input: event,
-    }),
+    body: x
   };
 }
